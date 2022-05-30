52f08e334f99aab633e35267a40ff5197b49ed6c
SrdjanLakic <srdjan949@gmail.com>
2022-05-30 13:12:42 -0400

    isLocked added

--- script.js
+++ script.js
@@ -10,11 +10,13 @@ let result = 0;
 let hitPosition;
 let currentTime = 10;
 let timerId = null;
+let isLocked = false;
 
 const randomSquare = function () {
   squares.forEach((square) => {
     square.classList.remove("mole");
   });
+  isLocked = false;
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
 
@@ -22,32 +24,32 @@ const randomSquare = function () {
 };
 
 squares.forEach((square) => {
-  square.addEventListener("mousedown", () => {
+  square.addEventListener("click", () => {
     if (square.id === hitPosition) {
+      if (isLocked) return;
       result++;
       score.textContent = result;
+      square.classList.remove("mole");
+      isLocked = true;
       hitPosition = null;
     }
   });
 });
 
-const moveMole = function () {
+const moveMole = function (e) {
   timerId = setInterval(randomSquare, 800);
 };
 
 const countDownTimer = function () {
-  const tick = function () {
-    timeLeft.textContent = currentTime;
-    if (currentTime === 0) {
-      clearInterval(timer);
-      alert(`Time is up, your score is ${result}`);
-    }
-    currentTime--;
-  };
-  tick();
-  const timer = setInterval(tick, 1000);
-  currentTime = 10;
+  timeLeft.textContent = currentTime;
+  if (currentTime === 0) {
+    clearInterval(countDownTimerId);
+    clearInterval(timerId);
+    alert(`Time is up, your final score is ${result}`);
+  }
+  currentTime--;
 };
+let countDownTimerId = setInterval(countDownTimer, 1000);
 
 startMole.addEventListener("click", function () {
   moveMole();
